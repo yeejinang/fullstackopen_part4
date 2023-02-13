f90783f01897c36d61900b44a87059b23f99722f
Yee Jin <yeejin0406@gmail.com>
2023-02-13 17:06:21 +0800

    done till ex 4.5

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -1,7 +1,29 @@
 const dummy = (blogs) => {
-	// ...
+	return (1)
   }
   
+  const totalLikes = (blogs) => {
+	const totalLikes = blogs.reduce(
+		(sum, blog) => sum + blog.likes, 0
+	)
+	return (totalLikes)
+  }
+
+  const favoriteBlog = (blogs) => {
+	if (blogs.length === 0)
+		return null
+
+	let favorite = blogs.reduce(
+		(prev, next) => next.likes > prev.likes ? next : prev
+	)
+
+	return {
+		title: favorite.title,
+		author: favorite.author,
+		likes: favorite.likes
+	}
+  }
+
   module.exports = {
-	dummy
+	dummy, totalLikes, favoriteBlog
   }
